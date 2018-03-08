70d5b3120ddab22facd47e2823a9521e14f754f9
t4009107 <ryan.west@elephant-room.info>
2018-03-08 11:42:46 +0000

    removed redundant js file

--- assets/js/bustime.js
+++
@@ -1,4 +0,0 @@
-const departureTime = "13:20".split(':');
-const currentDateTime = new Date();
-const departureDateTime = new Date(currentDateTime.getFullYear(), currentDateTime.getMonth(), currentDateTime.getDate(), departureTime[0], departureTime[1]);
-console.log(departureDateTime);
