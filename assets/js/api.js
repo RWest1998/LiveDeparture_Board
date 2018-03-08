9fafd722a54f7c4b1e42dfd9323d56a6b2790664
t4009107 <ryan.west@elephant-room.info>
2018-03-08 11:42:19 +0000

    Update readme, tweaked breakpoint styling and added in reload of data every minute to keep it up to date

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,4 @@
+// Hooking into api
 const fetchData = function() {
     return fetch('/static/data/data.json', {
         headers: {
@@ -11,40 +12,56 @@ const fetchData = function() {
             return json;
         });
 };
+
+//auto reload the data every minute to keep the bus timetable up to date
+const intervalID = setInterval(function() {
+    fetchData().then(function(data) {
+        processResults(data);
+    });
+
+}, 60000);
+
 fetchData()
     .then(function(data) {
-        //display the stop name
-        const stopName = data.stop_name;
-        document.getElementById('stop').innerHTML = stopName + " Bus Timetable";
-
-        const filteredItems = data.departures.filter(function(item){
-            //splitting the departure time up into separate hours and minutes
-            let departureTime = item.time.split(':');
-            //Current date and time
-            let currentDateTime = new Date();
-            //defining the departure time in a readable format
-            let departureDateTime = new Date(currentDateTime.getFullYear(), currentDateTime.getMonth(), currentDateTime.getDate(), departureTime[0], departureTime[1]);
-            //if statement to ignore Elm Row as a destination and ignore departures after the current time
-            if (item.destination !== "Elm Row" && (currentDateTime <= departureDateTime)){
-                return item;
-            }
-        });
-
-        for(var index = 0; index < 15; index++) {
-            var output = '<div class="row">' +
-                '<div class="col-sm"> ' +
-                '<div>' + filteredItems[index].destination + '</div> ' +
-                '</div> ' +
-                '<div class="col-sm"> ' +
-                '<div>' + filteredItems[index].service_name + '</div> ' +
-                '</div> ' +
-                '<div class="col-sm"> ' +
-                '<div>' + filteredItems[index].time + '</div> ' +
-                '</div> ' +
-                '</div>';
-            document.getElementById('grid').innerHTML += output;
-        };
+        processResults(data);
     });
+
+function processResults(data) {
+    //display the stop name
+    const stopName = data.stop_name;
+    document.getElementById('stop').innerHTML = stopName + " Bus Timetable";
+
+    const filteredItems = data.departures.filter(function(item){
+        //splitting the departure time up into separate hours and minutes
+        let departureTime = item.time.split(':');
+        //Current date and time
+        let currentDateTime = new Date();
+        //defining the departure time in a readable format
+        let departureDateTime = new Date(currentDateTime.getFullYear(), currentDateTime.getMonth(), currentDateTime.getDate(), departureTime[0], departureTime[1]);
+        //if statement to ignore Elm Row as a destination and ignore departures after the current time
+        if (item.destination !== "Elm Row" && (currentDateTime <= departureDateTime)){
+            return item;
+        }
+    });
+
+//HTML structure of the grid
+    let gridrows = '';
+    for(var index = 0; index < 10; index++) {
+        output = '<div class="row">' +
+            '<div class="col-sm"> ' +
+            '<div>' + filteredItems[index].destination + '</div> ' +
+            '</div> ' +
+            '<div class="col-sm"> ' +
+            '<div>' + filteredItems[index].service_name + '</div> ' +
+            '</div> ' +
+            '<div class="col-sm"> ' +
+            '<div>' + filteredItems[index].time + '</div> ' +
+            '</div> ' +
+            '</div>';
+        gridrows += output;
+    }
+    document.getElementById('grid-rows').innerHTML = gridrows;
+}
 
 
 
